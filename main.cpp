2618bdbcf527ce436cedd321c3578a97b15c7947
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-02-10 09:48:49 -0800

    now it actually solves and puts it into a more normal row echelon form (thus saving precious processing power)

--- main.cpp
+++ main.cpp
@@ -3,57 +3,71 @@
 #include <sstream>
 #include <vector>
 #include <deque>
+#include <iomanip>
 
-void rowOp(std::array<std::array<float, 3>, 3>& a, int r, float m) {
-    for (int i = 0; i < a[r].size(); i++) {
-        a[r][i] *= m;
-    }
-    std::cout << a[0][0] << "\n\n";
-}
-
-void rowOp(std::array<std::array<float, 3>, 3>& a, int r, int r1, float m) {
-        for (int i = 0; i < a[r].size(); i++) {
-            a[r][i] -= a[r1][i] * m;
-    } 
-}
-    
-
-void gauss(std::array<std::array<float, 3>, 3>& a) {
-    rowOp(a, 0, 1 / a[0][0]);
-    std::vector<float> sol;
-    for (int i = 1; i < a.size(); i++) {
-        for (int j = 0; j <= i-1; j++) {
-            rowOp(a, i, j, a[i][j]/a[j][j]);
-        }
-        rowOp(a, i, 1 / a[i][i]);
-    }
-}
+using namespace std;
 
 void printArr(std::array<std::array<float, 3>, 3> a) {
     for (int i = 0; i < a.size(); i++) {
         for (int j = 0; j < a[i].size(); j++) {
-            std::cout << a[i][j]<<" ";
+            std::cout << std::fixed << std::setprecision(2) << a[i][j] << " ";
         }
-        std::cout <<"\n";
+        std::cout << "\n";
     }
 }
 void printArr(std::deque<float> d) {
-    std::cout << "\n";
+    std::cout /* << std::fixed << std::setprecision(2)*/ << "\n";
     for (int i = 0; i < d.size(); i++) {
         std::cout << d[i] << " ";
     }
 }
 
+void rowOp(std::array<std::array<float, 3>, 3>& a, int r, float m, float constants[3]) {
+    for (int i = 0; i < a[r].size(); i++) {
+        a[r][i] = std::round(std::round((m * a[r][i]) * 1000.f) / 10.f) / 100.f;
+    }
+    constants[r] = std::round(std::round((m * constants[r]) * 1000.f) / 10.f) / 100.f;
+    //std::cout << a[0][0] << "\n\n";
+}
+
+void rowOp(std::array<std::array<float, 3>, 3>& a, int r, int r1, float m, float constants[3]) {
+    for (int i = 0; i < a[r].size(); i++) {
+            a[r][i] = std::round(std::round((a[r][i] - a[r1][i] * m) * 1000.f) / 10.f) / 100.f;
+    } 
+    constants[r] = std::round(std::round((constants[r] - constants[r1] * m) * 1000.f) / 10.f) / 100.f;
+    //std::cout<<"const: " << constants[r] << "\n";
+    printArr(a);
+}
+    
+
+void gauss(std::array<std::array<float, 3>, 3>& a, float constants[3]) {
+    //rowOp(a, 0, 1 / a[0][0], constants);
+    std::vector<float> sol;
+    for (int i = 1; i < a.size(); i++) {
+        for (int j = 0; j <= i-1; j++) {
+            std::cout << (a[i][j] / a[j][j]) << "\n";
+            rowOp(a, i, j, a[i][j]/a[j][j], constants);
+        }
+        //rowOp(a, i, 1 / a[i][i], constants);
+    }
+}
+
 std::deque<float> solve(std::array<std::array<float, 3>, 3> a, float constants[3]) {
+    std::cout << "constants: \n";
+    for (int i = 0; i < 3; i++) {
+        std::cout<<" "<< constants[i];
+    }
+    std::cout << "\n";
     std::deque<float> solutions;
-    solutions.push_front(constants[a.size() - 1]);
+    solutions.push_front(constants[a.size() - 1]/a[a.size() - 1][a.size() - 1]);
     for (int i = a.size() - 2; i >=0; i--) {
         float sum = 0;
         for (int j = i + 1, n = 0; j < a[i].size(); j++) {
             sum += a[i][j] * solutions[n];
             n++;
         }
-        solutions.push_front(constants[i]/sum);
+
+        solutions.push_front((constants[i]-sum)/(a[i][i]));
     }
     return solutions;
 }
@@ -65,7 +79,7 @@ int main() {
       {5, 2, 9},  
       {10, 8, 6}
     } };
-    gauss(a);
+    gauss(a, constants);
     printArr(a);
     std::deque<float> solutions = solve(a, constants);
     
